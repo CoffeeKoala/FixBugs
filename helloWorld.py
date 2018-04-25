34efdd301b918484a5512e47634e985908b0bb46
Zhang <coffeekoala@hotmail.com>
2018-04-25 12:29:54 +0800

    ci
    sayhello

---
+++ helloWorld.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: zhangminshu
+# @Date:   2018-04-25 12:27:10
+# @Email:  zhangminshu02@ppdai.com
+# @Last Modified by:   zhangminshu02
+# @Last Modified time: 2018-04-25 12:29:18
+################################################################################
+
+import time
+
+
+now = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+print "..............................."
+print "hello world!\n","current time is", now
+print "\nmy response is limited, you need to ask correct question"
+print "..............................."
