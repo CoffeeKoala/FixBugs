52f7f5641b2762cf47ff9495ea7451463c91c7a5
Zhang <coffeekoala@hotmail.com>
2020-08-03 13:43:29 +0800

    Create tempfile.py

---
+++ tempfile.py
@@ -0,0 +1,7 @@
+print("{str1:.<20}{count:.>5}".format(str1 = 'Fetch Row Count'   ,count = 120))
+print("{str1:.<20}{count:.>5}".format(str1 = 'Total Result Count',count = 20))
+
+output
+
+Fetch Row Count.......120
+Total Result Count.....20
