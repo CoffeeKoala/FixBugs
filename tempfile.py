72ec3b9007e8cbf7863b9d9bf17cf475eb989c43
Zhang <coffeekoala@hotmail.com>
2020-10-28 16:31:00 +0800

    Update tempfile.py

--- tempfile.py
+++ tempfile.py
@@ -5,3 +5,12 @@ output
 
 Fetch Row Count.......120
 Total Result Count.....20
+
+
+
+N = 4
+x = pd.qcut(df[var],N)
+bins = x.dtype.categories.values.to_tuples()
+labels = [ str(i) + "_"+ str(bins[i]) for i in range(0,N)]
+df[var+ '_bin'] =  pd.qcut(df[var],N,labels = labels)
+df[var+'_bin'].value_counts().sort_index()
